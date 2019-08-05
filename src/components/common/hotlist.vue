b51a7dafceeb41d0507f7cfdc710de311c4b04f5
zhouzhi <icitic-zhouzhi@csmail.citicbank.com>
2019-08-05 16:08:58 +0800

    new add reset css

--- src/components/common/hotlist.vue
+++ src/components/common/hotlist.vue
@@ -6,9 +6,10 @@
       <div class="content">
         <p>{{item.title}}</p>
         <p>{{item.desc| getxx}}</p>
-        <p class="detail"><router-link to="">查看详情</router-link></p>
+        <p class="detail"><router-link to="/home/hotcity">查看详情</router-link></p>
       </div>
     </div>
+    <!-- <router-view></router-view> -->
   </div>
 </template>
 <script>
--- src/router/index.js
+++ src/router/index.js
@@ -4,16 +4,28 @@ import Home from '@/pages/home/index'
 import Learn from '@/pages/learn/index'
 import Play from '@/pages/play/index'
 import My from '@/pages/my/index'
+import ChildHotCity from '@/pages/home/children/index'
 
 Vue.use(Router)
 
+const childs = [{
+  name: 'hotCity',
+  path:'/home/hotcity',
+  component: ChildHotCity
+}]
+
 export default new Router({
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  },
   routes: [
     {
       path: '/home',
       name: 'home',
-      component: Home
+      component: Home,
+      children:[...childs]
     },
+    
     {
       path: '/learn',
       name: 'learn',
